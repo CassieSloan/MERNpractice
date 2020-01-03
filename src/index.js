37244134c1b3e26e56be31d2b852c81a728f07c7
Cassie <cassandra.sloan16@gmail.com>
2020-01-03 13:03:59 +1100

    identified that express and nodemon were preventing rendering. Router and Mongo DB fine. Also created componants file in src with app and welcome.js + imported to index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from "react";
 import ReactDom from "react-dom"
-import Express from "express";
+// import Express from "express";
 import Mongoose from "mongoose";
 import Mongo from "mongodb";
+// import Nodemon from "nodemon";
+import Router from "react-router-dom";
+
+import App from "./componants/app";
+import Welcome from "./componants/welcome";
+
+ReactDom.render(
+    <App />,
+    document.querySelector("#root")
+);
 
