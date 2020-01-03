7e513ebd23eb0c930f7188190af059b67b3c9491
Cassie <cassandra.sloan16@gmail.com>
2020-01-03 13:11:10 +1100

    created welcome and list componants - successfully rendering

--- src/componants/app.js
+++ src/componants/app.js
@@ -1,10 +1,14 @@
 import React from "react";
+
+// componants
 import Welcome from "./welcome";
+import List from "./list";
 
 const App = () => {
     return (
         <>
             <Welcome/>
+            <List/>
         </>
     )   
 };
---
+++ src/componants/list.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const List = () => {
+    return (
+        <div> 
+            <p>This is our list container</p>
+        </div>
+    )
+}
+
+export default List;
